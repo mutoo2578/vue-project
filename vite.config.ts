7ad6f5fb57fba2de4705a5a4b18e46a011932f64
songzhenbo <13526614271@163.com>
2025-12-29 16:08:09 +0800

    测试

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: '/vue-project/',
   server: {
     host: '0.0.0.0',
     port: 3000,
